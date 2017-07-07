aedd99c46a3f1801fb93b9d01bb72a8c4d6dcf08
diego <diego2.riva@mail.polimi.it>
2017-07-07 17:35:57 +0200

    corrected some bugs

--- src/main/java/it/polimi/ingsw/pc42/View/OutputStringGenerator.java
+++ src/main/java/it/polimi/ingsw/pc42/View/OutputStringGenerator.java
@@ -120,7 +120,6 @@ public class OutputStringGenerator {
                             out.addAll(parseResources(immediateEffect));
                         }*/
                     }
-                    out.add("\n");
                 }
                 out.add("\n");
                 return out;
